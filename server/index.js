4385a4ee206383417ca7a40cc9bd44fbd1a38211
Brian Huston <hbriandrums@gmail.com>
2018-11-13 15:15:11 -0800

    Add else block

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.get('/api/movies/:genre/relatedmovies', (req, res) => {
   const param = req.params.genre.slice(1);
   db.getAll(param, (error, results) => {
     if (error) {
-      console.log(error);
       res.status(500).send(error.message);
+    } else {
+      res.send(results);
     }
-    res.send(results);
   });
 });
 
